10d54007f338dc81dee868487efc41e9d841a07b
Markus V <vogl91@gmail.com>
2021-12-04 08:08:22 +0100

    .

--- lib.py
+++ lib.py
@@ -1,4 +1,6 @@
+from typing import Tuple
 from aocd import submit, get_data
+from functools import partial
 
 def imag2tup(i: complex) -> tuple:  # 1+2j -> (1, 2)
     return (int(i.real), int(i.imag))
@@ -6,35 +8,22 @@ def imag2tup(i: complex) -> tuple:  # 1+2j -> (1, 2)
 def tup2imag(t: tuple) -> complex:
     return t[0] + t[1] * 1j
 
-def __level(method):
-    def annotation(day, quiet=True, print_res=True, do_example=True):
-        def solver(solve):
-            part = 'ab'[method]
-            td = testdata[day-1]
-            if(td and do_example):
-                if type(td) == list:
-                    esol = solve(td[method], method=method)
-                else:
-                    esol = solve(td, method=method)
-                if print_res:
-                    print(f"Ex. {day}{part}", esol, sep="\t")
-            sol = solve(get_data(day=day), method=method)
-            if print_res:
-                print(f"Lvl {day}{part}", sol, sep="\t")
-            submit(sol, part, day=day, quiet=quiet)
-            return solve # return the original function, so you can put another annotation on it
-        return solver
-    return annotation
+def bin2dec(x):
+    return int("".join(map(str, x)), 2)
 
-level_a, level_b = __level(0), __level(1)
-def level_ab(lvl):
+def level_ab(day: int, test: Tuple = None, methods=(0,1), quiet=False, print_res=True, sep="\n"):
     def solver(solve):
-        level_a(lvl)(solve)
-        level_b(lvl)(solve)
+        for method in methods:
+            # solve with testdata
+            if test and (actual := test[1+method]) != None:
+                result = solve(test[0].split(sep), method)
+                assert result == actual, f"Test failed {result} != {actual} with data\n{repr(test[0])}"
+            
+            # solve real
+            sol = solve(get_data(day=day).split(sep), method)
+            submit(sol, 'ab'[method], day=day, quiet=quiet)
+        return solve # return the original function, so you can put another annotation on it
     return solver
 
-def setdata(d):
-    testdata.clear()
-    testdata.extend(d)
-
-testdata = []
+level_a = partial(level_ab, methods=(0,))
+level_b = partial(level_ab, methods=(1,))
