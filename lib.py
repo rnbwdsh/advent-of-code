f210d21a62b28091fa09c7c1fad494ee7e212741
Markus V <vogl91@gmail.com>
2021-12-07 07:16:20 +0100

    .

--- lib.py
+++ lib.py
@@ -1,20 +1,24 @@
+from numpy import array
 from typing import Tuple
 from aocd import submit, get_data
 from functools import partial
 
-def level_ab(day: int, test: Tuple = None, methods=(0,1), quiet=False, print_res=True, sep="\n"):
+
+def level_ab(day: int, test: Tuple = None, levels=(0,1), quiet=False, print_res=True, sep="\n", apply=lambda a: a):
     def solver(solve):
-        for method in methods:
+        for level in levels:
+            parse = lambda dd: array([apply(d) for d in dd.split(sep)])
+            
             # solve with testdata
-            if test and (actual := test[1+method]) != None:
-                result = solve(test[0].split(sep), method)
+            if test and (actual := test[1+level]) != None:
+                result = solve(parse(test[0]), level)
                 assert result == actual, f"Test failed {result} != {actual} with data\n{repr(test[0])}"
             
             # solve real
-            sol = solve(get_data(day=day).split(sep), method)
-            submit(sol, 'ab'[method], day=day, quiet=quiet)
+            sol = solve(parse(get_data(day=day)), level)
+            submit(sol, 'ab'[level], day=day, quiet=quiet)
         return solve # return the original function, so you can put another annotation on it
     return solver
 
-level_a = partial(level_ab, methods=(0,))
-level_b = partial(level_ab, methods=(1,))
+level_a = partial(level_ab, levels=(0,))
+level_b = partial(level_ab, levels=(1,))
