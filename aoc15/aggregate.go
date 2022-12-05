057b7a5d6d03e85dbf1f051c16e55d6a699590d7
m <vogl91@gmail.com>
2022-12-05 23:32:19 +0100

    Add 2015 in golang

---
+++ aoc15/aggregate.go
@@ -0,0 +1,41 @@
+package aoc15
+
+// ignore unused
+func MinOf(vars ...int) int {
+	if len(vars) == 0 {
+		return -1
+	}
+	min := vars[0]
+	for _, i := range vars {
+		if min > i {
+			min = i
+		}
+	}
+	return min
+}
+
+func MaxOf(vars ...int) int {
+	max := vars[0]
+	for _, i := range vars {
+		if max < i {
+			max = i
+		}
+	}
+	return max
+}
+
+func Sum(vars ...int) int {
+	sum := 0
+	for _, i := range vars {
+		sum += i
+	}
+	return sum
+}
+
+func Map[T, V any](ts []T, fn func(T) V) []V {
+	result := make([]V, len(ts))
+	for i, t := range ts {
+		result[i] = fn(t)
+	}
+	return result
+}
