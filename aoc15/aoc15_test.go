a00a2bb2487cb012ec9cbc3f6fcb923edf79a2e6
m <vogl91@gmail.com>
2022-12-06 02:15:16 +0100

    Move fun

--- aoc15/aoc15_test.go
+++ aoc15/aoc15_test.go
@@ -38,23 +38,25 @@ func readBodyWithCookie(req *http.Request, client *http.Client) string {
 	return string(body)[:len(body)-1]
 }
 
+var fun = []func(string) int{
+	level1a, level1b, level2a, level2b, level3a, level3b, level4a, level4b, level5a, level5b,
+	level6a, level6b, level7a, level7b, level8a, level8b, level9a, level9b, level10a, level10b,
+	level11a, level11b, level12a, level12b, level13a, level13b, level14a, level14b, level15a, level15b,
+	level16a, level16b, level17a, level17b, level18a, level18b, level19a, level19b, level20a, level20b,
+	level21a, level21b, level22a, level22b, level23a, level23b, level24a, level24b, level25a, level25b,
+}
+
 func TestFirst(t *testing.T) {
-	functions := []func(string) int{
-		level1a, level1b, level2a, level2b, level3a, level3b, level4a, level4b, level5a, level5b,
-		level6a, level6b, level7a, level7b, level8a, level8b, level9a, level9b, level10a, level10b,
-		level11a, level11b, level12a, level12b, level13a, level13b, level14a, level14b, level15a, level15b,
-		level16a, level16b, level17a, level17b, level18a, level18b, level19a, level19b, level20a, level20b,
-		level21a, level21b, level22a, level22b, level23a, level23b, level24a, level24b, level25a, level25b}
-	for day := 1; day <= len(functions)/2; day++ {
+	for day := 1; day <= len(fun)/2; day++ {
 		data := readDay(day)
 		idx := (day - 1) * 2
 
 		// submit a
-		var resp = functions[idx](data)
+		var resp = fun[idx](data)
 		println(day, "a", resp)
 
 		// submit b
-		resp = functions[idx+1](data)
+		resp = fun[idx+1](data)
 		println(day, "b", resp)
 	}
 }
